c9cdcda69a2d40c221ed4c3fc59ab3ee16cb063e
jw-bakerross <james.williamson@bakerross.co.uk>
2025-07-22 10:43:23 +0100

    BakerGPT rename and branding fixed

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -62,9 +62,9 @@ function AuthLayout({
       <BlinkAnimation active={isFetching}>
         <div className="mt-6 h-10 w-full bg-cover">
           <img
-            src="/assets/br-wide.png"
+            src="/assets/br-logo.svg"
             className="h-full w-full object-contain"
-            alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? 'LibreChat' })}
+            alt={localize('com_ui_logo', { 0: startupConfig?.appTitle ?? 'BakerGPT' })}
           />
         </div>
       </BlinkAnimation>
